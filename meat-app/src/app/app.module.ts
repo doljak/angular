0a79d9c789374435fa5c5e85a315e6449d890538
Jefferson <jedoljak@gmail.com>
2018-09-17 22:58:25 -0300

    start meat app header / home

---
+++ meat-app/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
+
+
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './src/app/header/header.component';
+import { HomeComponent } from './src/app/home/home.component'
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpModule,
+    RouterModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
