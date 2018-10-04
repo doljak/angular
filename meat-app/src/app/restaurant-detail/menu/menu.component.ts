ee07c94264b9f2dcae9c52f336dffe57581d65d9
Jefferson <jedoljak@gmail.com>
2018-10-04 20:26:14 -0300

    menu component

--- meat-app/src/app/restaurant-detail/menu/menu.component.ts
+++ meat-app/src/app/restaurant-detail/menu/menu.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+
+import { RestaurantService } from '../../restaurants/restaurant/restaurant.service'
+import { ActivatedRoute } from '@angular/router'
+import { MenuItem } from '../menu-item/menu-item.model'
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'mt-menu',
@@ -6,9 +11,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MenuComponent implements OnInit {
 
-  constructor() { }
+  itens:Observable< MenuItem[] >
+
+  constructor(
+    private restaurantService:RestaurantService,
+    private route:ActivatedRoute
+  ) { }
 
   ngOnInit() {
+    this.itens = this.restaurantService.menuOfRestaurantById(this.route.parent.snapshot.params['id'])
+  }
+
+  addItem(item:MenuItem)
+  {
+    console.log(item)
   }
 
 }
