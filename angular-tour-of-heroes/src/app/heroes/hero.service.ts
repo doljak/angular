ee9c049bb122031de29b958713210f6e3288ed4b
Jefferson de Melo Doljak <Jefferson.de.Melo@ibm.com@mbpdejeersonibm.partner.bs.br.bsch>
2018-10-15 17:43:07 -0300

    hero service

---
+++ angular-tour-of-heroes/src/app/heroes/hero.service.ts
@@ -0,0 +1,18 @@
+import { Hero } from './heroes.class'
+import { HEROES } from '../../mock-heroes'
+
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { HttpClient } from 'selenium-webdriver/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HeroService {
+
+  constructor() { }
+
+  getHeroes():Observable< Hero[] > {
+    return of ( HEROES )
+  }
+}
