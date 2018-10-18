18c3dab70c21dd20d3816e2993860c6cb9e27ab8
Jefferson de Melo Doljak <Jefferson.de.Melo@ibm.com@MacBook-Pro-de-Jefferson-ibm.local>
2018-10-17 22:37:04 -0300

    routing heroes details

--- angular-tour-of-heroes/src/app/heroes/hero.service.ts
+++ angular-tour-of-heroes/src/app/heroes/hero.service.ts
@@ -1,24 +1,25 @@
-import { Hero } from './heroes.class'
-import { HEROES } from '../../mock-heroes'
-
 import { Injectable } from '@angular/core';
+
 import { Observable, of } from 'rxjs';
+
+import { Hero } from './heroes.class';
+import { HEROES } from '../../mock-heroes';
 import { MessagesService } from '../messages/messages.service';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable({ providedIn: 'root' })
 export class HeroService {
 
-  constructor(
-    public messageService:MessagesService
-  )
-  {}
+  constructor(private messageService: MessagesService) { }
 
-  getHeroes():Observable< Hero[] >
-  {
-    //TODO: sending message _after_ fetching service
-    this.messageService.add("Hero Service: fetched heroes")
-    return of ( HEROES )
+  getHeroes(): Observable<Hero[]> {
+    // TODO: send the message _after_ fetching the heroes
+    this.messageService.add('HeroService: fetched heroes');
+    return of(HEROES);
+  }
+
+  getHero(id: number): Observable<Hero> {
+    // TODO: send the message _after_ fetching the hero
+    this.messageService.add(`HeroService: fetched hero id=${id}`);
+    return of(HEROES.find(hero => hero.id === id));
   }
 }
