472a0ea822e5e9b05a78f106faa6f1e4eaf4e6f5
Jefferson <jedoljak@gmail.com>
2019-01-03 21:42:03 -0200

    update 2019

--- meat-app/src/app/restaurants/restaurant/restaurant.model.ts
+++
@@ -1,8 +0,0 @@
-export interface Restaurant{
-    id:string
-    name:string
-    category:string
-    rating:number
-    deliveryEstimate:string
-    imagePath:string
-}
--- typescript/dist/app.js
+++
@@ -1,7 +0,0 @@
-var message = "Hello world";
-console.log(message);
-var number = 5;
-console.log(number);
-var variable;
-variable = 'quando não tipada ela será do tipo :any';
-console.log(variable);
