487b936e372b6cb1033f4cbe82d29684e099f371
Jefferson <jedoljak@gmail.com>
2019-02-02 18:20:39 -0200

    Create restaurants component

--- codebr-angular/meat-app-starter/src/app/app.module.ts
+++ codebr-angular/meat-app-starter/src/app/app.module.ts
@@ -8,6 +8,7 @@ import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component'
 import { ROUTES } from './app.routes';
+import { RestaurantsComponent } from './restaurants/restaurants.component';
 
 
 @NgModule({
@@ -15,7 +16,8 @@ import { ROUTES } from './app.routes';
     AppComponent,
     HeaderComponent,
     HomeComponent,
-    AboutComponent
+    AboutComponent,
+    RestaurantsComponent
   ],
   imports: [
     BrowserModule,
