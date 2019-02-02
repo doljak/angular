eeb081ca5b0d3e83b47698a778cb54d911e303d1
Jefferson <jedoljak@gmail.com>
2019-02-02 18:41:40 -0200

    Remove files css and Spec

--- codebr-angular/meat-app-starter/src/app/app.module.ts
+++ codebr-angular/meat-app-starter/src/app/app.module.ts
@@ -9,6 +9,7 @@ import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component'
 import { ROUTES } from './app.routes';
 import { RestaurantsComponent } from './restaurants/restaurants.component';
+import { RestaurantComponent } from './src/app/restaurants/restaurant/restaurant.component';
 
 
 @NgModule({
@@ -17,7 +18,8 @@ import { RestaurantsComponent } from './restaurants/restaurants.component';
     HeaderComponent,
     HomeComponent,
     AboutComponent,
-    RestaurantsComponent
+    RestaurantsComponent,
+    RestaurantComponent
   ],
   imports: [
     BrowserModule,
