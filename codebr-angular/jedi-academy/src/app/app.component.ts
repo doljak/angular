dd16f6fb222bdb30079e2e7a10153c28ffac149b
Jefferson <jedoljak@gmail.com>
2019-02-02 19:15:23 -0200

    Remove css

--- codebr-angular/jedi-academy/src/app/app.component.ts
+++ codebr-angular/jedi-academy/src/app/app.component.ts
@@ -3,8 +3,7 @@ import { Component } from '@angular/core';
 import { Student } from './student/student.model'
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
   title = 'jedi-academy';
