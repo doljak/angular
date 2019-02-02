6f43e5ede6367f0f740b936e88cd4d9c48830c25
Jefferson <jedoljak@gmail.com>
2019-02-02 17:52:49 -0200

    Create about component + routes

--- codebr-angular/meat-app-starter/src/app/app.module.ts
+++ codebr-angular/meat-app-starter/src/app/app.module.ts
@@ -3,11 +3,11 @@ import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
 import { RouterModule } from '@angular/router';
 
-
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component'
+import { ROUTES } from './app.routes';
 
 
 @NgModule({
@@ -20,7 +20,7 @@ import { AboutComponent } from './about/about.component'
   imports: [
     BrowserModule,
     HttpModule,
-    RouterModule
+    RouterModule.forRoot(ROUTES)
   ],
   providers: [],
   bootstrap: [AppComponent]
