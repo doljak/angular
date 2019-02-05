48a1a977aa47977f9a9d243c80bb544f4fda03d5
Jefferson de Melo Doljak <Jefferson.de.Melo@ibm.com@MacBook-Pro-de-Jefferson-ibm.local>
2019-02-05 17:48:04 -0200

    reviews

--- codebr-angular/meat-app-starter/src/app/details-restaurant/reviews/reviews.component.ts
+++ codebr-angular/meat-app-starter/src/app/details-restaurant/reviews/reviews.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { Restaurant } from 'app/restaurants/restaurant/restaurant.model';
+import { RestaurantService } from 'app/restaurants/restaurant/restaurant.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'mt-reviews',
@@ -6,9 +10,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReviewsComponent implements OnInit {
 
-  constructor() { }
+  reviews:Observable<any>
+
+  constructor(
+    private restaurantService:RestaurantService,
+    private activeRoute:ActivatedRoute
+    ) { }
 
   ngOnInit() {
+    this.reviews = this.restaurantService.get('restaurant', this.activeRoute.parent.snapshot.params['id'], 'reviews')
   }
 
 }
