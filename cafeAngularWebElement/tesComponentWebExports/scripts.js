8b3edd7751b22beda93e1ee7584accfa16362668
Jefferson de Melo Doljak <Jefferson.de.Melo@ibm.com@MacBook-Pro-de-Jefferson-ibm.local>
2019-02-05 16:20:35 -0200

    Remove content old

--- cafeAngularWebElement/tesComponentWebExports/scripts.js
+++
@@ -1,7 +0,0 @@
-const letterPicker = document.querySelector('app-vowel-picker')
-
-console.log(letterPicker)
-
-letterPicker.addEventListener('letterSelected', function(e){
-    console.log(`letter with addEventListener:${e.detail}`)
-})
