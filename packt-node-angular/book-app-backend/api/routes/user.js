126bd2fb4a8419d0fee5f0ab49f793f04e4f5bec
Jefferson de Melo Doljak <Jefferson.de.Melo@ibm.com@MacBook-Pro-de-Jefferson-ibm.local>
2019-02-08 17:50:50 -0200

    Create routes for user and book

--- packt-node-angular/book-app-backend/api/routes/user.js
+++ packt-node-angular/book-app-backend/api/routes/user.js
@@ -53,6 +53,91 @@ router.post('/admin-signup', (req, res, next)=>{
   const email = req.body.email;
   User.find({email:email})
     .then(user => {
+      if(user.lenght > 1){
+        return res.status(409).json({
+          msg:'User already exist'
+        })
+      } else {
+        bcrypt.hash(req.body.password, 10, (err, hash) => {
+          if(err){
+            return res.status(500).json({
+              msg:'Failure to create user admin',
+              error: err,
+            })
+          } else {
+            const user = new User({
+              _id:new mongoose.Types.ObjectId(),
+              email:req.body.email,
+              password:hash,
+              name:req.body.name,
+              user_type:'admin',
+            })
+            user.save()
+            .then(res => {
+              return res.status(201).json({
+                msg:'User created successfully'
+              })
+            })
+            .catch(err => {
+              console.log(err);
+              return res.status(500).json({
+                msg: 'Admin creation failed!',
+                error:err,
+              })
+            })
 
+          };
+        });
+      };
+    });
+});
+
+router.post('/admin/login', (req,res, next) =>{
+  console.log(req.body.email);
+  console.log(req.body.password);
+  const email = req.body.email
+  User.find({email:email})
+    .exec()
+    .then(user => {
+      if(user.lenght < 1){
+        return res.status(401).json({
+          msg:'Auth failed'
+        });
+      };
+      const userFound = user[0]
+      bcrypt.compare(req.body.password, userFound, (err, result)=>{
+        if(err){
+          return res.status(500).json({
+            msg:'Auth failed'
+          });
+        }
+        if(result){
+          const token = jwt.sign({
+              email:userFound.email,
+              userId:userFound._id
+            },
+            'secret',
+            {
+              expiresIn:'1hr'
+            }
+          )
+          return res.status(200).json({
+            msg:"Auth user success",
+            user_type:userFound.user_type,
+            token:token,
+          })
+        };
+        return res.status(401).json({
+          msg: 'Auth failed'
+        });
+      })
     })
-})
+    .catch(err=>{
+      console.log(err)
+      return res.status(500).json({
+        msg:'Auth failed'
+      })
+    })
+});
+
+module.exports = router;
