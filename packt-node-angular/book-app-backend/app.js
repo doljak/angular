cdc70909010a039611f2c30d762b3096787deec5
Jefferson de Melo Doljak <Jefferson.de.Melo@ibm.com@MacBook-Pro-de-Jefferson-ibm.local>
2019-02-10 19:13:35 -0200

    Complete app

--- packt-node-angular/book-app-backend/app.js
+++ packt-node-angular/book-app-backend/app.js
@@ -1,4 +1,5 @@
-const app = require('express')();
+const express = require('express');
+const app = express();
 
 const bookRoutes = require('./api/routes/books');
 const userRoutes = require('./api/routes/user');
@@ -7,8 +8,48 @@ const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://jefferson.doljak@ibm.com:jeff!2143@my-first-cluster-svtck.mongodb.net/test?retryWrites=true');
+mongoose.connect(
+    'mongodb://localhost:27018/db_bookApp'
+);
 
 app.use(morgan('dev'));
-app.use(bodyParser.urlencoded({urlencoded:false}));
+app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
+
+app.use(function(req,res,next){
+    console.log('header-interceptor: Start setting headers.')
+	res.header("Access-Control-Allow-Origin" , "*");
+	res.header(
+		"Access-Control-Allow-Headers", "*"
+	);
+	if(req.method === 'OPTIONS'){
+        console.log('header-interceptor: ===OPTIONS.')
+		var headers = {};
+		headers["Access-Control-Allow-Methods"] = "POST, PATCH ,GET, PUT, DELETE, OPTIONS";
+		res.writeHead(200, headers);
+		res.end();
+        console.log('header-interceptor: Headers set.')
+	}
+    next();
+})
+
+app.use('/user', userRoutes);
+app.use('/books', bookRoutes);
+
+app.use((req,res,next) => {
+    const error = new Error("Not Found");
+    error.status = 404;
+    next(error);
+})
+
+app.use((error, req, res, next) => {
+    console.log('header-interceptor: error 500.')
+    res.status(error.status || 500);
+    res.json({
+        error:{
+            message:error.message
+        }
+    });
+});
+
+module.exports = app;
