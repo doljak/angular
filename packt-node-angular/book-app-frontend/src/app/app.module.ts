9c3eac3efabfcaeb42a345644bb396d9d9f8a06b
Jefferson de Melo Doljak <Jefferson.de.Melo@ibm.com@MacBook-Pro-de-Jefferson-ibm.local>
2019-02-11 12:41:23 -0200

    Basic Route Done in book app

--- packt-node-angular/book-app-frontend/src/app/app.module.ts
+++ packt-node-angular/book-app-frontend/src/app/app.module.ts
@@ -2,10 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './templates/login/login.component';
+import { SignupComponent } from './templates/signup/signup.component';
+import { BooksComponent } from './templates/books/books.component';
+import { AllBooksComponent } from './templates/all-books/all-books.component';
+import { AddBooksComponent } from './templates/add-books/add-books.component';
+import { EditBooksComponent } from './templates/edit-books/edit-books.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SignupComponent,
+    BooksComponent,
+    AllBooksComponent,
+    AddBooksComponent,
+    EditBooksComponent
   ],
   imports: [
     BrowserModule
